b019308af4ef8961a8efe235c7ecf0b1ec139a7f
akio0911 <akio0911@gmail.com>
2016-05-15 12:09:25 +0900

    Add copyable protocol sample

--- MyPlayground.playground/Pages/Untitled Page 5.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page 5.xcplaygroundpage/Contents.swift
@@ -2,6 +2,31 @@
 
 import Foundation
 
-var str = "Hello, playground"
 
-//: [Next](@next)
+protocol Copyable {
+    func copy() -> Self
+}
+
+class Data: Copyable {
+    required init() {}
+    
+    func copy() -> Self {
+        return self.dynamicType.init()
+    }
+}
+
+class SpecialData: Data {}
+
+let data1: Data        = Data().copy()
+let data2: SpecialData = SpecialData().copy()
+
+data1.dynamicType // Data.Type
+data2.dynamicType // SpecialData.Type
+
+let copyable: Copyable = data2
+
+// error: cannot convert value of type 'Copyable' to specified type 'SpecialData'
+// let copy1: SpecialData = copyable.copy() // NG 🙅🏻
+
+let copy2: Copyable = copyable.copy()
+
