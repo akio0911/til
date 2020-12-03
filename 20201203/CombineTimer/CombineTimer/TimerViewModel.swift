7c006b675528f50bf279b376f1aab4ac98015418
akio0911 <akio0911@gmail.com>
2020-12-03 19:12:14 +0900

    TimerViewModel実装

--- 20201203/CombineTimer/CombineTimer/TimerViewModel.swift
+++ 20201203/CombineTimer/CombineTimer/TimerViewModel.swift
@@ -6,7 +6,47 @@
 //
 
 import Foundation
+import Combine
 
-final class TimerViewModel {
+protocol TimerViewModelInput {
+    func tappedPauseButton()
+    func tappedResetButton()
+    func tappedStartButton()
+}
+
+protocol TimerViewModelOutput {
+    var currentSecondsPublisher: AnyPublisher<String?, Never> { get }
+}
+
+protocol TimerViewModelType {
+    var input: TimerViewModelInput { get }
+    var output: TimerViewModelOutput { get }
+}
+
+final class TimerViewModel: TimerViewModelType, TimerViewModelInput, TimerViewModelOutput {
+    private let model: TimerModel
     
+    var input: TimerViewModelInput { self }
+    var output: TimerViewModelOutput { self }
+    
+    init(model: TimerModel = TimerModel()) {
+        self.model = model
+        self.currentSecondsPublisher = model.countPublisher
+            .map { String(format: "%02d:%02i:%02i", $0 / 3600 % 60, $0 / 60 % 60, $0 % 60) }
+            .eraseToAnyPublisher()
+    }
+    
+    func tappedPauseButton() {
+        model.pauseTimer()
+    }
+    
+    func tappedResetButton() {
+        model.resetTimer()
+    }
+    
+    func tappedStartButton() {
+        model.startTimer()
+    }
+    
+    let currentSecondsPublisher: AnyPublisher<String?, Never>
 }
