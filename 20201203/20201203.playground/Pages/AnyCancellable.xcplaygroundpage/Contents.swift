71da08af0473bb0d1818f332478bbedbe2b783e0
akio0911 <akio0911@gmail.com>
2020-12-03 15:47:30 +0900

    cancelサンプル追加

---
+++ 20201203/20201203.playground/Pages/AnyCancellable.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+//: [Previous](@previous)
+
+import Foundation
+import Combine
+
+let publisher = PassthroughSubject<Int, Never>()
+
+let cancellable = publisher
+    .handleEvents(receiveCancel: {
+        print("receive cancel")
+    })
+    .sink(receiveCompletion: {
+        print($0)
+    }, receiveValue: {
+        print($0)
+    })
+
+publisher.send(1)
+publisher.send(2)
+
+cancellable.cancel()
+
+publisher.send(3)
+
+
+
+//: [Next](@next)
