dbcd2756b909ad6b345d8142a8f389d9219ce436
akio0911 <akio0911@gmail.com>
2020-12-03 14:18:12 +0900

    FailTests追加

--- 20201203/CombineExample/CombineExampleTests/CombineExampleTests.swift
+++ 20201203/CombineExample/CombineExampleTests/CombineExampleTests.swift
@@ -6,28 +6,32 @@
 //
 
 import XCTest
+import Combine
 @testable import CombineExample
 
-class CombineExampleTests: XCTestCase {
+class FailTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    enum ExampleError: Error {
+        case example
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test() {
+        let sink = expectation(description: "")
+        
+        let publisher = Fail(outputType: Void.self, failure: ExampleError.example)
+        
+        let _ = publisher.sink(receiveCompletion: {
+            switch $0 {
+            case .finished:
+                XCTFail("エラーが流れなかった")
+            case .failure(let error):
+                XCTAssertEqual(error, ExampleError.example)
+                sink.fulfill()
+            }
+        }, receiveValue: { _ in
+            XCTFail("値が流れてしまった")
+        })
+        
+        wait(for: [sink], timeout: 0.01)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
