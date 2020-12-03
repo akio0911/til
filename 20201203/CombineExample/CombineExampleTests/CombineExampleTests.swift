54d294207c8ae7362201f0f8df79dddbcc3fb123
akio0911 <akio0911@gmail.com>
2020-12-03 15:03:17 +0900

    MapErrorTestsを追加

--- 20201203/CombineExample/CombineExampleTests/CombineExampleTests.swift
+++ 20201203/CombineExample/CombineExampleTests/CombineExampleTests.swift
@@ -9,6 +9,39 @@ import XCTest
 import Combine
 @testable import CombineExample
 
+class MapErrorTests: XCTestCase {
+    enum ExampleError: Error { case example }
+    enum OtherError: Error { case example }
+    
+    func testエラーを他のエラーにして異常終了() {
+        let sink = expectation(description: "")
+        sink.expectedFulfillmentCount = 1
+
+        let publisher = Fail(
+            outputType: Void.self,
+            failure: ExampleError.example
+        )
+        
+        let _ = publisher
+            .mapError({ error -> OtherError in
+                OtherError.example
+            })
+            .sink(receiveCompletion: {
+                switch $0 {
+                case .finished:
+                    XCTFail("正常終了してしまった")
+                case .failure(let error):
+                    XCTAssertEqual(error, OtherError.example)
+                    sink.fulfill()
+                }
+            }, receiveValue: {
+                XCTFail("値を受け取ってはいけない")
+            })
+        
+        wait(for: [sink], timeout: 0.01)
+    }
+}
+
 class CatchTests: XCTestCase {
     enum ExampleError: Error { case example }
 
