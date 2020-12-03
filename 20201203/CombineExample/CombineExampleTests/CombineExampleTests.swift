8570bf4a9d1432c7836a0ed829217cea2210ec57
akio0911 <akio0911@gmail.com>
2020-12-03 14:57:49 +0900

    CatchTests追加

--- 20201203/CombineExample/CombineExampleTests/CombineExampleTests.swift
+++ 20201203/CombineExample/CombineExampleTests/CombineExampleTests.swift
@@ -9,6 +9,71 @@ import XCTest
 import Combine
 @testable import CombineExample
 
+class CatchTests: XCTestCase {
+    enum ExampleError: Error { case example }
+
+    func testエラーを置き換える() {
+        let sink = expectation(description: "")
+        sink.expectedFulfillmentCount = 2
+        
+        let publisher = Fail(
+            outputType: String.self,
+            failure: ExampleError.example
+        )
+
+        let _ = publisher
+            .catch { _ in
+                Just("X")
+            }
+            .sink(receiveCompletion: {
+                switch $0 {
+                case .finished:
+                    sink.fulfill()
+                case .failure:
+                    XCTFail("エラーが流れてしまい異常終了")
+                }
+            }, receiveValue: {
+                XCTAssertEqual($0, "X")
+                sink.fulfill()
+            })
+        
+        wait(for: [sink], timeout: 0.01)
+    }
+    
+    func testリトライと組み合わせる() {
+        let sink = expectation(description: "")
+        sink.expectedFulfillmentCount = 1
+        
+        var temp: [Int] = []
+        
+        let _ = (1..<100).publisher
+            .tryMap({ (arg: Int) -> Int in
+                if arg == 3 {
+                    throw ExampleError.example
+                }
+                return arg
+            })
+            .retry(1)
+            .catch({ error -> AnyPublisher<Int, Never> in
+                print(error)
+                return Just(0).eraseToAnyPublisher()
+            })
+            .sink(receiveCompletion: {
+                switch $0 {
+                case .finished:
+                    sink.fulfill()
+                case .failure:
+                    XCTFail("エラーが流れてしまい異常終了n")
+                }
+            }, receiveValue: {
+                temp.append($0)
+            })
+
+        XCTAssertEqual(temp, [1,2,1,2,0])
+        wait(for: [sink], timeout: 0.01)
+    }
+}
+
 class ReplaceErrorTests: XCTestCase {
     enum ExampleError: Error { case example }
     
