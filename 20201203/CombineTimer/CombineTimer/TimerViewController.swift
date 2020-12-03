550f8f5ebd775ef73efd12abfcb2f7ae70cd28df
akio0911 <akio0911@gmail.com>
2020-12-03 19:52:13 +0900

    TimerViewController実装

--- 20201203/CombineTimer/CombineTimer/TimerViewController.swift
+++ 20201203/CombineTimer/CombineTimer/TimerViewController.swift
@@ -6,23 +6,44 @@
 //
 
 import UIKit
+import Combine
 
 class TimerViewController: UIViewController {
 
     @IBOutlet private weak var secondsLabel: UILabel!
     
+    private var viewModel: TimerViewModelType!
+    private var cancelables = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureNavigation()
+        bindViewModel()
     }
 
     @IBAction func reset(_ sender: Any) {
+        viewModel.input.tappedResetButton()
     }
     
     @IBAction func start(_ sender: Any) {
+        viewModel.input.tappedStartButton()
     }
     
     @IBAction func pause(_ sender: Any) {
+        viewModel.input.tappedPauseButton()
+    }
+    
+    private func configureNavigation() {
+        navigationItem.title = "タイマー"
+    }
+    
+    private func bindViewModel() {
+        let viewModel = TimerViewModel()
+        self.viewModel = viewModel
+        
+        viewModel.currentSecondsPublisher
+            .assign(to: \.text, on: secondsLabel)
+            .store(in: &cancelables)
     }
 }
 
