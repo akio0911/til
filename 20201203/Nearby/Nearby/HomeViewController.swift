0addaba33fe49f1eda24102cd30eda0bd1b2e4e9
akio0911 <akio0911@gmail.com>
2020-12-07 22:40:52 +0900

    HomeViewControllerを追加

---
+++ 20201203/Nearby/Nearby/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  Nearby
+//
+//  Created by akio0911 on 2020/12/07.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+}
---
+++ 20201203/Nearby/Nearby/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  Nearby
+//
+//  Created by akio0911 on 2020/12/07.
+//
+
+import Foundation
+
+final class HomeViewModel {
+    
+}
