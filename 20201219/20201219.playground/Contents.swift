9e5a8a72c5ceb2a1a470078ca217f69245e32122
akio0911 <akio0911@gmail.com>
2020-12-20 18:57:54 +0900

    merge系を追加

--- 20201219/20201219.playground/Contents.swift
+++ 20201219/20201219.playground/Contents.swift
@@ -135,3 +135,180 @@ func combineLatest() {
     pub2.send(12)
     pub1.send(13)
 }
+
+// combineLatest(_:) | Apple Developer Documentation
+// https://developer.apple.com/documentation/combine/publisher/combinelatest(_:)
+func combineLatestOther() {
+    let pub1 = PassthroughSubject<Int, Never>()
+    let pub2 = PassthroughSubject<Int, Never>()
+    
+    let cancellable = pub1
+        .combineLatest(pub2)
+        .sink(receiveValue: {print($0)})
+    
+    pub1.send(1)
+    pub1.send(2)
+    pub2.send(2)
+    pub1.send(3)
+    pub1.send(45)
+    pub2.send(22)
+}
+
+func combineLatest3Transform() {
+    let pub = PassthroughSubject<Int, Never>()
+    let pub2 = PassthroughSubject<Int, Never>()
+    let pub3 = PassthroughSubject<Int, Never>()
+    
+    let cancellable = pub
+        .combineLatest(pub2, pub3, {
+            return $0 * $1 * $2
+        })
+        .sink(receiveValue: {print($0)})
+    
+    pub.send(1)
+    pub.send(2)
+    pub2.send(2)
+    pub3.send(10)
+    
+    pub.send(9)
+    pub3.send(4)
+    pub2.send(12)
+}
+
+func combineLatest3() {
+    let pub = PassthroughSubject<Int, Never>()
+    let pub2 = PassthroughSubject<Int, Never>()
+    let pub3 = PassthroughSubject<Int, Never>()
+    
+    let cancellable = pub
+        .combineLatest(pub2, pub3)
+        .sink(receiveValue: {print($0)})
+    
+    pub.send(1)
+    pub.send(2)
+    pub2.send(2)
+    pub3.send(9)
+    
+    pub.send(3)
+    pub2.send(12)
+    pub.send(13)
+    pub3.send(19)
+}
+
+func combineLatest4Transform() {
+    let pub = PassthroughSubject<Int, Never>()
+    let pub2 = PassthroughSubject<Int, Never>()
+    let pub3 = PassthroughSubject<Int, Never>()
+    let pub4 = PassthroughSubject<Int, Never>()
+    
+    let cancellable = pub
+        .combineLatest(pub2, pub3, pub4, {
+            return $0 * $1 * $2 * $3
+        })
+        .sink(receiveValue: {print($0)})
+    
+    pub.send(1)
+    pub.send(2)
+    pub2.send(2)
+    pub3.send(9)
+    pub4.send(1)
+    
+    pub.send(3)
+    pub2.send(12)
+    pub.send(13)
+    pub3.send(19)
+}
+
+func combineLatest4() {
+    let pub = PassthroughSubject<Int, Never>()
+    let pub2 = PassthroughSubject<Int, Never>()
+    let pub3 = PassthroughSubject<Int, Never>()
+    let pub4 = PassthroughSubject<Int, Never>()
+    
+    let cancellable = pub
+        .combineLatest(pub2, pub3, pub4)
+        .sink(receiveValue: {print($0)})
+    
+    pub.send(1)
+    pub.send(2)
+    pub2.send(2)
+    pub3.send(9)
+    pub4.send(1)
+    
+    pub.send(3)
+    pub2.send(12)
+    pub.send(13)
+    pub3.send(19)
+}
+
+// merge(with:) | Apple Developer Documentation
+// https://developer.apple.com/documentation/combine/publisher/merge(with:)-7qt71
+func merge() {
+    let pub = PassthroughSubject<Int, Never>()
+    let pub2 = PassthroughSubject<Int, Never>()
+    
+    let cancellable = pub.merge(with: pub2)
+        .sink(receiveValue: {print($0)})
+    
+    pub.send(1)
+    pub.send(2)
+    pub2.send(2)
+    
+    pub.send(3)
+    pub2.send(12)
+    pub.send(13)
+}
+
+// merge(with:_:) | Apple Developer Documentation
+// https://developer.apple.com/documentation/combine/publisher/merge(with:_:)
+func merge3() {
+    let pubA = PassthroughSubject<Int, Never>()
+    let pubB = PassthroughSubject<Int, Never>()
+    let pubC = PassthroughSubject<Int, Never>()
+    
+    let cancellable = pubA
+        .merge(with: pubB, pubC)
+        .sink(receiveValue: {print($0)})
+    
+    pubA.send(1)
+    pubB.send(40)
+    pubC.send(90)
+    pubA.send(2)
+    pubB.send(50)
+    pubC.send(100)
+}
+
+// merge(with:_:_:_:_:_:_:) | Apple Developer Documentation
+// https://developer.apple.com/documentation/combine/publisher/merge(with:_:_:_:_:_:_:)
+func merge8() {
+    let pubA = PassthroughSubject<Int, Never>()
+    let pubB = PassthroughSubject<Int, Never>()
+    let pubC = PassthroughSubject<Int, Never>()
+    let pubD = PassthroughSubject<Int, Never>()
+    let pubE = PassthroughSubject<Int, Never>()
+    let pubF = PassthroughSubject<Int, Never>()
+    let pubG = PassthroughSubject<Int, Never>()
+    let pubH = PassthroughSubject<Int, Never>()
+    
+    let cancellable = pubA
+        .merge(with: pubB, pubC, pubD, pubE, pubF, pubG, pubH)
+        .sink(receiveValue: {print($0)})
+    
+    pubA.send(1)
+    pubB.send(40)
+    pubC.send(90)
+    pubD.send(-1)
+    pubE.send(33)
+    pubF.send(44)
+    pubG.send(54)
+    pubH.send(1000)
+    
+    pubA.send(2)
+    pubB.send(50)
+    pubC.send(100)
+    pubD.send(-2)
+    pubE.send(33)
+    pubF.send(33)
+    pubG.send(54)
+    pubH.send(1001)
+}
